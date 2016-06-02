4735d14caa1c038c08863e593d327e9d7fe2f042
Your Name <you@example.com>
2016-06-02 12:07:08 +0530

    Added newline

--- boto.py
+++ boto.py
@@ -5,6 +5,7 @@ s3 = boto3.resource('s3')
 # for bucket in s3.buckets.all():
 #     print (bucket.name)
 
+
 #s3.create_bucket(Bucket='mybucket38')
 #s3.Object('mybucket38','hello.txt').put(Body=open())
 
